ed9088675f156806531b7f69907d891c62e4b984
shimeng <shimeng@ifenqu.com>
2017-03-28 15:51:37 +0800

    update

--- backsit/movie/models.py
+++ backsit/movie/models.py
@@ -4,80 +4,6 @@ import django
 from django.db import models
 
 
-# Create your models here.
-
-# 电影详情
-class MovieDetail(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    sub_name = models.CharField(max_length=100, null=True)
-    rating = models.FloatField(default=0)
-    rating_sum = models.IntegerField(default=0)
-    release_time = models.DateField(null=True, blank=True, default=datetime.date.today())
-    about = models.TextField(null=True)
-    content = models.TextField(null=True)
-    tag = models.CharField(max_length=100, null=True)
-    area = models.CharField(max_length=100, null=True)
-    # movie_type = models.
-    status = models.SmallIntegerField(default=1)
-    create_time = models.TimeField(auto_created=True, default=django.utils.timezone.now)
-    source = models.CharField(max_length=100, null=True)
-
-    def get_desc(self):
-        return self.content[0:20] + "..."
-
-    def datetime_format(self):
-        return datetime.datetime.strftime(self.release_time, "%Y-%m-%d")
-
-    class Meta:
-        db_table = 'movie_detail'
-
-
-# class MovieContent(models.Model):
-#     id = models.AutoField(primary_key=True)
-
-
-# 电影海报图片
-class MovieImages(models.Model):
-    id = models.AutoField(primary_key=True)
-    movie = models.ForeignKey(MovieDetail, null=True)
-    image = models.CharField(max_length=500)
-    # 图片类型 1：海报  2：剧照
-    img_type = models.SmallIntegerField(default=1, null=True)
-
-    class Meta:
-        db_table = 'movie_images'
-
-
-# 电影下载地址
-class MovieDownloadUrl(models.Model):
-    id = models.AutoField(primary_key=True)
-    movie = models.ForeignKey(MovieDetail, null=True)
-    download_url = models.CharField(max_length=1000)
-
-    class Meta:
-        db_table = 'movie_download_url'
-
-
-class MovieType(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    # 删除字段
-    status = models.SmallIntegerField(default=1)
-
-    class Meta:
-        db_table = "movie_type"
-
-
-class MovieTypeDetail(models.Model):
-    id = models.AutoField(primary_key=True)
-    movie = models.ForeignKey(MovieDetail)
-    movie_type = models.ForeignKey(MovieType)
-
-    class Meta:
-        db_table = "movie_type_detail"
-
-
 # 敏感词词库
 class SensitiveWords(models.Model):
     id = models.AutoField(primary_key=True)
@@ -87,3 +13,24 @@ class SensitiveWords(models.Model):
 
     class Meta:
         db_table = "sensitive_words"
+
+
+class ProductInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=50)
+    product_type = models.ForeignObject
+
+    class Meta:
+        db_table = "product_info"
+
+
+class ProductType(models.Model):
+    id = models.AutoField(primary_key=True)
+    key = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    describe = models.CharField(max_length=200, null=True)
+    status = models.SmallIntegerField()
+    create_time = models.DateTimeField()
+
+    class Meta:
+        db_table = "product_type"
